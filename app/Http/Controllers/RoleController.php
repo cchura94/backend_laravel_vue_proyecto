b8eb0852f4f184def2dfb561363083bd57296ceb
cristian <cchura.cpc@gmail.com>
2025-03-10 20:09:55 -0400

    Proyecto Base (CRUD Usuarios)

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // funListar
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // funGuardar
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        // funMostrar
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        // funModificar
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // funEliminar
+    }
+}
---
+++ docs/git/comandos-git.md
@@ -0,0 +1,20 @@
+# Comandos de GIT (Iniciales)
+## Requerimientos
+- Descargar GIT (https://git-scm.com/)
+- Crear una cuenta en GITHUB (https://github.com)
+## Configurar GIT 
+- ejecutar los siguientes comandos
+```
+git config --global user.name "Su Nombre"
+git config --global user.email "su.correo@mail.com"
+```
+## Comandos inciales
+## Crear un repositorio en GITHUB
+## Inicializar un repositorio Local
+```
+git init
+```
+### Referencia del respositorio (Local) al repositorio (remoto)
+```
+git remote add origin https://github.com/cchura94/backend_laravel_vue_proyecto.git
+```
